46f67de846ea44df9a57ebf5409287c60f1c7b22
HugoCambier <32418537+HugoCambier@users.noreply.github.com>
2017-10-14 19:00:26 +0200

    Add files via upload
    UP

--- TP2/src/main/java/TMatrix.java
+++ TP2/src/main/java/TMatrix.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -18,57 +19,53 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class TMatrix {
 
     public static class TokenizerMapper
-            extends Mapper<IntWritable, Text, IntWritable, MapWritable>{
+            extends Mapper<LongWritable, Text, LongWritable, MapWritable>{
 
-        private IntWritable col = new IntWritable(0);
+        private LongWritable col = new LongWritable(0);
         private MapWritable cell = new MapWritable();
 
-        public void map(IntWritable key, Text value, Context context
+        public void map(LongWritable key, Text value, Context context
         ) throws IOException, InterruptedException {
 
             // Split line into cells
             String[] cells = value.toString().split(",");
 
-            StringTokenizer itr = new StringTokenizer(cells.toString());
-            while (itr.hasMoreTokens()) {
+            for(int i=0; i<cells.length; i++){
 
-                IntWritable line = new IntWritable(key.get());
+                // The key is the column index, which will become the line index
+                col.set(i);
 
                 // Current line which will become the new column index
-                cell.put(new Text("line"), line);
+                cell.put(new Text("line"), new LongWritable(key.get()));
                 // Value of the cell
-                cell.put(new Text("value"), new Text(itr.nextToken()));
+                cell.put(new Text("value"), new Text(cells[i]));
 
-                // Write current column which will become the new line index
                 // (column, (line, value))
                 context.write(col, cell);
-
-                // Increment column index
-                col.set(col.get()+1);
             }
         }
     }
 
     public static class IntTransposeReducer
-            extends Reducer<IntWritable, MapWritable, IntWritable, Text> {
+            extends Reducer<LongWritable, MapWritable, LongWritable, Text> {
 
         private Text result = new Text();
 
-        public void reduce(IntWritable key, Iterable<MapWritable> values,
+        public void reduce(LongWritable key, Iterable<MapWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
             // Generate a new line: CSV line
-            StringBuilder line = new StringBuilder();
+            Text line = new Text();
 
             // Construct the array with all cells of a line (initialisation)
-            ArrayList<MapWritable> cells = new ArrayList<MapWritable>();
+            ArrayList<MapWritable> cells = new ArrayList<>();
 
             // Fill the array cells
             for (MapWritable val : values) {
                 MapWritable cell = new MapWritable();
 
                 // Receive the new position: new column = ex line
-                IntWritable newCol = (IntWritable) val.get(new Text("line"));
+                LongWritable newCol = (LongWritable) val.get(new Text("line"));
 
                 // Receive the value
                 Text cellValue = (Text) val.get(new Text("value"));
@@ -79,23 +76,27 @@ public class TMatrix {
             }
 
 
-            int indexCol = 0;
-            // Write in the new line
+            // Need to sort cells in the line
+            cells.sort(Comparator.comparingLong(a -> ((LongWritable) a.get(new Text("col"))).get()));
+
+            /*
+            Now, we can run the line because cells are sorted
+            Write in the new line, cell by cell
+            */
+            int s = cells.size()-1;
+            int i =0;
             for (MapWritable cell : cells){
-                int newCol = ((IntWritable) cell.get(new Text("position"))).get();
-                // Write in respect of the initial position
-                if (newCol == indexCol) {
-                    Text cellText = (Text) cell.get(new Text("value"));
-                    // Add cell value to the new line
-                    line.append(cellText).append(",");
+                Text cellText = (Text) cell.get(new Text("value"));
+                // Add cell value to the new line
+                line.append(cellText.getBytes(),0,cellText.getLength());
+                // Put a "," between values,
+                // To not write "," after the last cell
+                if (s > i) {
+                    line.append(new Text(",").getBytes(), 0, 1);
                 }
-                indexCol++;
+                i++;
             }
 
-            // Remove last character because it's a ","
-            if(line.length() > 0){
-                line.setLength(line.length() - 1);
-            }
 
             result.set(line.toString());
 
@@ -110,9 +111,7 @@ public class TMatrix {
         job.setMapperClass(TokenizerMapper.class);
         job.setReducerClass(IntTransposeReducer.class);
 
-        //job.setOutputKeyClass(IntWritable.class);
-        //job.setOutputValueClass(MapWritable.class);
-        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(MapWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
